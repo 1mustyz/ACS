9e3500178407eaa7d9f8ed858ce8ae4c68453a09
zayyadadamu <zayyadadamu1@gmail.com>
2021-11-19 10:35:41 +0100

    pusher

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -4,12 +4,15 @@ const pusher = new Pusher({
     app_id: "1236911",
     key: "2e43b248a65f500c42e1",
     secret: "d420b43dabbda03f1610",
-    cluster: "mt1"
+    cluster: "mt1",
+    useTLS: true
 })
 
-exports.manageAlert = (req,res,next) => {
+exports.manageAlert =  (req,res,next) => {
 
-        pusher.trigger("notifications", "alert", {
-            message: "hello world"
+       pusher.trigger("notifications", "alert", {
+            message: "hello tapaswi"
           });
+
+          res.json({h:"jdjdj"})
 }
