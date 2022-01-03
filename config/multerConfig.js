5bb9f9031ed5df4258b2818b357d3ad4fe71499c
1mustyz <onemusty.z@gmail.com>
2022-01-03 22:38:33 +0100

    cloudinary

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-      cb(null, 'public/images');
+      cb(null, '');
   },
 
   // By default, multer removes file extensions so let's add them back
